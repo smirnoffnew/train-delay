d2d799fe8bb6a5cffb13cd12d1babade56515b90
smirnoffnew <smirnoffnew@gmail.com>
2019-12-06 10:11:30 +0200

    Updates

---
+++ precache-manifest.689f4e6bf5a47c871f078444028e58d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a4ce4b76d7530b3583b4c83401cce7",
+    "url": "/train-delay/index.html"
+  },
+  {
+    "revision": "dda6c981dfcdbc4170e7",
+    "url": "/train-delay/static/css/main.36e5d5ec.chunk.css"
+  },
+  {
+    "revision": "d7dd50497fbc0216ce95",
+    "url": "/train-delay/static/js/2.3f0103e6.chunk.js"
+  },
+  {
+    "revision": "dda6c981dfcdbc4170e7",
+    "url": "/train-delay/static/js/main.a284bdfb.chunk.js"
+  },
+  {
+    "revision": "4cb28320b2791bc8d1d8",
+    "url": "/train-delay/static/js/runtime-main.4098fdcd.js"
+  }
+]);
